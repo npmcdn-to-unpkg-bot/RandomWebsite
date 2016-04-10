516b81c3304435fbe2e52aed3a4ed518534e43a6
Robert Harvey Moura <rhmoura1@gmail.com>
2016-04-10 16:19:00 +1000

    Added Online and Lan Features

--- index.js
+++ index.js
@@ -1,27 +1,170 @@
-var app = angular.module("app", ["ui.router"]);
+var app = angular.module("app", ["ui.router", "toastr"]);
 
 app.config(function($urlRouterProvider, $stateProvider) {
-	$stateProvider.state("StartScreen", {url:"", templateUrl:"startScreen.html"})
-	$stateProvider.state("gameScreen", {url:"/play", templateUrl:"gameScreen.html"})
+	// $stateProvider.state("StartScreen", {url:"", templateUrl:"startScreen.html"})
+	$stateProvider.state("gameScreen", {url:"", templateUrl:"gameScreen.html"})
 });
 
-app.controller("appController", ["$scope", function($scope, $location){
+app.factory("peerService", function() {
+	var skylink = new Skylink();
+	var connectedId;
+	var connectedPeerId;
+	var thisRef = this;
+	thisRef.connected = false;
+	skylink.init({
+		apiKey: '9d1fc96f-fa91-468a-ae03-851ffb89eb59',
+		defaultRoom: 'test'
+	}, function() {
+		skylink.joinRoom({
+		});
+	});
+
+	thisRef.onMessage = function(fn) {
+		skylink.on('incomingMessage', function(messageData, unknown, sender, isSelf) {
+			if (!isSelf) {
+				fn((messageData.content))
+			}
+		});
+	}
+
+	thisRef.send = function(message) {
+		skylink.sendMessage(message, connectedPeerId);
+	}
+
+	thisRef.onJoin = function(fn) {
+		skylink.on('peerJoined', function(peerId, peerInfo, isSelf) {
+			if (!isSelf) {
+				thisRef.connected = true;
+				connectedPeerId = peerId;
+
+				fn(connectedId > connectedPeerId)
+			} else {
+				connectedId = peerId;
+			}
+
+		});
+	}
+
+	return thisRef;
+})
+
+app.controller("appController", ["$scope", "toastr", "peerService", function($scope, toastr, peerService){
+	$scope.peerService = peerService;
 	$scope.squares = [{state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}];
-	$scope.player = "fa fa-times";
+	var players = [{symbol: "X", class: "fa fa-times"}, {symbol: "O", class: "fa fa-circle-o"}];
+	var player;
+	var myTurn;
+
+	$scope.turnMessage = "It's your opponnents turn";
+
+	peerService.onJoin(function(isHigher) {
+		if (isHigher === true) {
+			player = players[0]
+			myTurn = true;
+			$scope.turnMessage = "It's your turn";
+		} else {
+			player = players[1]
+			myTurn = false;
+			$scope.turnMessage = "It's your opponnents turn";
+		}
+
+		$scope.$evalAsync();
+	});
 
 	$scope.editSquare = function(index){
-		if ($scope.squares[index].state == "") {
-			$scope.squares[index].state = $scope.player;
-			if ($scope.player == "fa fa-times") {
-				$scope.player = "fa fa-circle-o";
-			} else {
-				$scope.player = "fa fa-times";
+		if (myTurn) {
+			if ($scope.squares[index].state === "") {
+				$scope.squares[index].state = player.class;
+
+				peerService.send($scope.squares);
+				var winner = setWinner()
+				if (winner.isWinner === true) {
+					peerService.send(winner.name);
+					setTimeout(function() {
+						$scope.squares = [{state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}];
+						$scope.$evalAsync();
+					}, 1000);
+				}
+
+				myTurn = false;
+				$scope.turnMessage = "It's your opponnents turn";
+				$scope.$evalAsync();
 			}
 		}
 	};
 
-	$scope.checkWinner = function() {
+	peerService.onMessage(function(message) {
+		if (angular.isString(message) === true){
+			setWinner(message);
+			myTurn = false;
+			setTimeout(function() {
+				$scope.squares = [{state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}, {state: ""}];
+				$scope.$evalAsync();
+			}, 1000);
+		} else {
+			$scope.squares = message;
+			$scope.$evalAsync();
+			myTurn = true;
+			$scope.turnMessage = "It's your turn";
+		}
 
+		$scope.$evalAsync();
+	});
+
+	var setWinner = function(playerWinner) {
+		if (playerWinner === undefined) {
+			var winner = checkWinner();
+			if (winner.isWinner) {
+				var winName;
+				if (winner.winner == "fa fa-times") {
+					winName = "X";
+				} else {
+					winName = "O"
+				}
+
+				toastr.success("Good job " + winName, "Player " + winName + " has won!")
+				return {isWinner: true, name: winName};
+			}
+		} else {
+			toastr.success("HAHA YOU LOST TO PLAYER " + playerWinner + "!");
+		}
+
+		return {isWinner: false};
+	}
+
+	var checkWinner = function() {
+		var squares = $scope.squares;
+		for (i = 0; i < 3; i++) {
+			if (checkEqual(i, i + 3, i + 6)) {
+				return {isWinner: true, winner: squares[i].state};
+			}
+		}
+
+		for (i = 0; i < 9; i += 3) {
+			if (checkEqual(i, i + 1, i + 2)) {
+				return {isWinner: true, winner: squares[i].state};
+			}
+		}
+
+		if (checkEqual(2, 4, 6)) {
+			return {isWinner: true, winner: squares[2].state};
+		}
+
+		if (checkEqual(0, 4, 8)) {
+			return {isWinner: true, winner: squares[0].state};
+
+		}
+
+		return {isWinner: false, winner: ""};
 	};
+
+	var checkEqual = function(a, b, c) {
+		var squares = $scope.squares;
+		if (squares[a].state !== "" && squares[a].state == squares[b].state && squares[a].state == squares[c].state) {
+			return true
+		}
+
+		return false
+	}
 
 }]);
